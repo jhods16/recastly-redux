f48adecd1526220e3017ba70ba4f44fde11e6d98
jhods16 <jessicarhodson@gmail.com>
2018-11-28 17:41:38 -0800

    Implement action creator functions and current video reducer

--- src/actions/search.js
+++ src/actions/search.js
@@ -3,10 +3,20 @@ import changeVideoList from './videoList.js';
 import changeVideo from './currentVideo.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
 
-
+//refactor to be a fetch (somewhere), switch port on reduxMocks to 8080.
 var handleVideoSearch = (q) => {
- 
+  var options = {
+    key: YOUTUBE_API_KEY,
+    query: q,
+    max: 5
+  };
   //TODO:  Write an asynchronous action to handle a video search!
+  return (dispatch) => {
+    searchYouTube(options, (data) => {
+      dispatch(changeVideoList(data));
+      dispatch(changeVideo(data[0]));
+    });
+  };
 };
 
 export default handleVideoSearch;
