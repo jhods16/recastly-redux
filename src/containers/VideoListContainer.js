567cb62839eb3c509d0e5b0e17980d82e724fabd
jhods16 <jessicarhodson@gmail.com>
2018-11-28 20:53:09 -0800

    Pseudocode VideoListContainer

--- src/containers/VideoListContainer.js
+++ src/containers/VideoListContainer.js
@@ -2,9 +2,21 @@ import { connect } from 'react-redux';
 import VideoList from './../components/VideoList.js';
 import changeVideo from './../actions/currentVideo.js';
 
-var VideoListContainer = () => {};
+// var VideoListContainer = () => {
+  
+// var videos = state.videoList;
+  
+//  var handleVideoListEntryTitleClick = (dispatch) => {
+//     return {
+//       onClick: (video) => dispatch(changeVideo(video))
+//     };
+//   };
+
+// };
 
 //TODO: define a VideoListContainer component which will hook up your action
 // dispatchers with your VideoList component props.
+
+
 
 export default VideoListContainer;
